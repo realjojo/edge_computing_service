f2fb610b21865423ebff0770fd8e4152a6b63bd3
realjojo <593474624@qq.com>
2019-04-02 17:59:01 +0800

    04.02修改

--- src/test/java/com/ecs/EdgeComputingServiceApplicationTests.java
+++ src/test/java/com/ecs/EdgeComputingServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EdgeComputationServiceApplicationTests {
+public class EdgeComputingServiceApplicationTests {
 
     @Test
     public void contextLoads() {
